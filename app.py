ea7701384ecf24204f6713f1807fb7a87858f583
Geheim01 <nils.wenzlitschke@googlemail.com>
2023-05-15 11:02:22 +0200

    fix search

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
-data = pd.read_csv("board_games_atlas.csv")
+data = pd.read_csv("data/board_games_atlas.csv")
 
 @app.route('/')
 def index():
@@ -15,30 +15,37 @@ def games():
     sortby = request.args.get('sortby', default='name')
     order = request.args.get('order', default='asc')
     search = request.args.get('search', default='')
+    category = request.args.get('categories', default='')
+    mechanic = request.args.get('mechanic', default='')
+    players = request.args.get('players', default='')
 
     # Filter the data based on search query
     if search:
-        print("search triggered with: ", search)
         filtered_data = data[data['name'].str.contains(search, case=False)]
     else:
         filtered_data = data
 
+    # Filter the data based on category, mechanic, and players
+    if category:
+        filtered_data = filtered_data[filtered_data['categories'] == category]
+    if mechanic:
+        filtered_data = filtered_data[filtered_data['mechanics'] == mechanic]
+    if players:
+        filtered_data = filtered_data[filtered_data['min_players'].astype(str) <= players]
+        filtered_data = filtered_data[filtered_data['max_players'].astype(str) >= players]
+
     # Sort the data
     sorted_data = filtered_data.sort_values(by=sortby, ascending=(order == 'asc'))
 
-    # Convert the data to a list of dictionaries
-    games_list = sorted_data.to_dict('records')
+    # Replace NaN values with None and convert the data to a list of dictionaries
+    games_list = sorted_data.fillna(value='ffill').to_dict('records')
 
-    for i in games_list:
-        print(i['name'])
     # Return the games data as JSON if the request is AJAX
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
         return jsonify(games_list)
 
     # Render the template with the games data
     return render_template('games.html', games=games_list, sortby=sortby, order=order, search=search)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
